bf6e41ac777e6ebe899d1f5b3f61ff12158dbc14
dandancardoso <daniel.cardoso91@live.com>
2015-03-16 10:32:27 -0300

    primeiro commit

---
+++ GitFuncionando/ImprimirTchau.swift
@@ -0,0 +1,17 @@
+//
+//  ImprimirTchau.swift
+//  AulaGit
+//
+//  Created by Douglas Mandarino on 3/12/15.
+//  Copyright (c) 2015 Douglas Mandarino. All rights reserved.
+//
+
+import Foundation
+
+class ImprimirTchau{
+ //teste    
+    func imprimir()
+    {
+        println("Tchau!!!")
+    }
+}
---
+++ GitFuncionando/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  GitFuncionando
+//
+//  Created by Douglas Mandarino on 3/16/15.
+//  Copyright (c) 2015 Douglas Mandarino. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+//         Do any additional setup after loading the view, typically from a nib.
+        
+                var ola = ImprimirOla();
+                var tchau = ImprimirTchau();
+        
+                ola.imprimir()
+                tchau.imprimir()
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+}
+
